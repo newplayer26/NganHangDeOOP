c238cc91df843fdd2d8629764b778d0a08187bb9
DESKTOP-L19PFEH\Zero910 <luonghoainam03@gmail.com>
2023-07-05 00:22:04 +0700

    ExportToPdf

--- NganHangDe/Services/IFileService.cs
+++ NganHangDe/Services/IFileService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NganHangDe.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace NganHangDe.Services
 {
     public interface IFileService
     {
+        byte[] GeneratePdf(List<QuestionModel> questions);
+        void SavePdfFile(byte[] pdfData);
         Task<string> AddQuestionsByFile(string filePath, int categoryId);
     }
 }
