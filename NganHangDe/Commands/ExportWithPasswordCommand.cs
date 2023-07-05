ce3aae775c696d38821a8091d07ba6ce7566959b
DESKTOP-L19PFEH\Zero910 <luonghoainam03@gmail.com>
2023-07-05 13:22:49 +0700

    Update

--- NganHangDe/Commands/ExportWithPasswordCommand.cs
+++ NganHangDe/Commands/ExportWithPasswordCommand.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using NganHangDe.Models;
+using NganHangDe.ModelsDb;
+using NganHangDe.Services;
+using NganHangDe.ViewModels.StartupViewModels;
+using NganHangDe.ViewModels.TabbedNavigationTabViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NganHangDe.Commands
 {
-    internal class ExportWithPasswordCommand
+    public class ExportWithPasswordCommand : AsyncCommandBase
     {
+        private QuizPageViewModel _viewmodel;
+        private FileService _fileService = new FileService();
+        private QuizService _quizService = new QuizService();
+        public ExportWithPasswordCommand(QuizPageViewModel viewmodel)
+        {
+            _viewmodel = viewmodel;
+        }
+        public override async Task ExecuteAsync(object parameter)
+        {
+            List<QuestionModel> questionmodels = new List<QuestionModel>();
+            if (_viewmodel.IsShuffleChecked == false)
+            {
+                questionmodels = await _quizService.GetAllQuestionsFromQuizAsync(_viewmodel.Quiz.Id);
+            }
+            else questionmodels = _viewmodel.ShuffledQuestionList.ToList();
+            var pdfData = _fileService.GeneratePdf(questionmodels, _viewmodel.Password);
+            _fileService.SavePdfFile(pdfData);
+        }
     }
 }
